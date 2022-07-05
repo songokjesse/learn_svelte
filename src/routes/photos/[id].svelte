bca22aa7e6c164127040af50b584c409b0755891
Jesse Songok <jessesongok@gmail.com>
2022-07-05 14:01:39 +0300

    added dynamic route

--- src/routes/photos/[id].svelte
+++ src/routes/photos/[id].svelte
@@ -1,6 +1,32 @@
-<script>
+<script context="module">
+    // @ts-ignore
+    export async function load({fetch, params}) {
+        let id = params.id
+        const res = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
+        const photo = await res.json()
 
+        if(res.ok) {
+            return {
+            props : {
+                photo
+            }
+        }
+        return {
+           status: res.status,
+           error: new Error("Photos not found")
+        }
+        
+    }
+}
+</script>
+<script>
+// @ts-nocheck
+    export let photo
 </script>
 <div>
     <h1>Photo</h1>
+    <div>
+        <h2>{photo.title}</h2>
+        <img src={photo.url} alt=""/>
+    </div>
 </div>
